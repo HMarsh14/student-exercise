c3d960564d8806752f72229a44185c289e431ddb
HMarsh14 <hmarsh1409@gmail.com>
2025-11-26 10:12:32 +0000

    Edited edit_entry functional test

--- tests/acceptance_tests/functional_tests.py
+++ tests/acceptance_tests/functional_tests.py
@@ -103,6 +103,6 @@ class FunctionalTests(TestCase):
         self.dsl.confirm_name_already_exists_validation_error()
 
     def test_can_edit_entry(self):
-        self.dsl.ensure_existing_entry(name="Old")
+        self.dsl.ensure_existing_entry()
         self.dsl.update_existing_entry(current_name="Old", new_name="New")
         self.dsl.confirm_entry_updated(old_name="Old", new_name="New")
